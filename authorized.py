9af61d407242a7a8459e01dad3619b6830df41a9
Darren Wee <darrenweezy@gmail.com>
2016-06-06 19:09:10 +0800

    updated authorized module with more chat IDs & refactoring
    refactored user groups to be stored in a dictionary instead of lone lists; added getAll and getGroups functions to fetch IDs for all recipients or respective groups

--- authorized.py
+++ authorized.py
@@ -6,29 +6,52 @@
 
 # dictionary for Telegram chat IDs
 address_book = {
-    'Darren'    : 53558212,     # black VOGL, admin
+    # FOP comm
+    'Andrea'    : 113764188,    # FOP director
+    'Shane'     : 89686519,     # oweek director
+    'Deborah'   : 222836171,    # events director
     'Yantyng'   : 112279032,    # safety/FOP, black OGL
+
+    # USC MC
+    'Tham'      : 111665525,    # USC MC member
+
+    # VOGLs
+    'Darren'    : 53558212,     # black VOGL, admin
     'Chester'   : 110971462,    # blue VOGL
     'Samantha'  : 106888349,    # red VOGL
     'Khaiqing'  : 118410662,    # purple VOGL
     'Claire'    : 27374797,     # green VOGL
     'Jiahao'    : 68976391,     # orange VOGL
+
+    # COGLs
+    'Xinying'   : 104784604,    # black COGL
+    'Changming' : 52917741,     # blue COGL
+    'Natalya'   : 16335747,     # purple COGL
+    'Bryan'     : 117396861,    # green COGL
+    'Hongwei'   : 98195170,     # orange COGL
 }
 
 rev_book = {v: k for k, v in address_book.items()}
 
 # user groups
-admins = ['Darren']
-vogls = ['Darren', 'Jiahao', 'Claire', 'Khaiqing', 'Chester', 'Samantha']
-cogls = []
-fopcomm = ['Yantyng']
-safety = ['Darren', 'Yantyng']
+global groups
+groups = {
+	'admins'	: ['Darren'],
+	'vogls'	    : ['Darren', 'Jiahao', 'Claire', 'Khaiqing', 'Chester', 'Samantha'],
+	'cogls'	    : ['Xinying', 'Changming', 'Natalya', 'Bryan', 'Hongwei'],
+	'fopcomm'	: ['Andrea', 'Shane', 'Deborah', 'Yantyng', 'Tham'],
+	'safety'	: ['Darren', 'Yantyng'],
+}
 
-##### all mailing_list users are stored here #####
-#mailing_list = admins + vogls + cogls + fopcomm + safety
-mailing_list = ['Darren']
-#mailing_list = []
-mailing_list = list(set(mailing_list)) # get rid of duplicates
+def getAll():
+    return list(set(getGroups(['vogls', 'cogls', 'fopcomm'])))
+
+def getGroups(groupList):
+    IDs = []
+    for group in groupList:
+        if group in groups:
+            IDs += getIDs(groups.get(group))
+    return IDs
 
 def getIDs(group):
     IDs = []
@@ -42,3 +65,9 @@ def whoIs(target_id):
         return rev_book.get(target_id)
     else:
         return target_id
+
+##### all mailing_list users are stored here #####
+#mailing_list = admins + vogls + cogls + fopcomm + safety + usc
+#mailing_list = ['Darren', 'Andrea', 'Tham']
+#mailing_list = []
+mailing_list = getAll()
