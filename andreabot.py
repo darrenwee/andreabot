a6fa5f4176b720ce299d975d5849e710b3f55a1c
darrenwee <darrenweezy@gmail.com>
2016-06-07 05:10:18 +0800

    implemented read and status functions
    added global dictionary to track message acknowledgement; experimental

--- andreabot.py
+++ andreabot.py
@@ -27,6 +27,12 @@ logging.captureWarnings(True)
 global announcements
 announcements = []
 
+global message_id
+message_id = 1
+
+global read
+read = {}
+
 @bot.message_handler(commands = ['start'])
 def welcome(message):
     logMessage(message)
@@ -79,36 +85,48 @@ def yell(message):
     # deny people who aren't in the mailing list from yelling
     if message.from_user.id not in getMailingList():
         logger.warning('Attempted unauthorized use of /yell by %s' % message.from_user.id)
-        bot.reply_to(message, 'Sorry! You aren\'t allowed to use /yell. Tell Darren (@ohdearren) if this is a mistake.')
+        bot.reply_to(message, 'Sorry! You aren\'t allowed to use /yell because you are not on the mailing list. Tell Darren (@ohdearren) if this is a mistake.')
         return
 
     # build the timestamp (modify format as necessary)
     # timestamp is pretty and human-readable, 12hr + date
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%I:%M%p, %d %B %Y')
 
+    # attach message ID
+    broadcast = 'Message ID. %d\n' % message_id
+
     # strip command word
-    #broadcast = '----- MESSAGE BEGIN -----\n' + re.sub('^\/yell\s+', '', message.text)
-    broadcast = re.sub('^/yell\s+', '', message.text)
+    broadcast += re.sub('^/yell\s+', '', message.text)
 
     # append byline
-    #broadcast += '\n----- MESSAGE END -----\n' + '\nSent by %s\n@%s' % (whoIs(message.from_user.id), message.from_user.username)
     broadcast += '\n\nSent by %s\n@%s' % (whoIs(message.from_user.id), message.from_user.username)
     broadcast += '\n' + timestamp
 
+    # store in global list and read dictionary
     announcements.append(broadcast)
+    read[message_id] = {}
+
+    # trivial catch
+    read[message_id][whoIs(message.from_user.id)] = True
+
     failed = ''
     # send to all recipients
     for recipient in getMailingList():
         try:
             logger.info('Yelling at \'%s\': \'%s\'' % (whoIs(recipient), re.sub('^/yell\s+', '', message.text)))
             bot.send_message(recipient, broadcast)
+            read[message_id][whoIs(recipient)] = False
         except Exception:
             logger.warn('Failed to yell at \'%s\'' % whoIs(recipient))
             failed += whoIs(recipient) + '\n'
+            read[message_id][whoIs(recipient)] = 'Failed'
 
     # feedback to sender about failed recipients
     if failed != '':
         bot.reply_to(message, '\nWARNING: yell did not reach the following people\n\n%s' % failed)
+
+    message_id += 1
+    return
 
 """
     /log
@@ -160,6 +178,91 @@ def logMessage(message):
 def getWho(message):
     logMessage(message)
     bot.reply_to(message, enumerateListeners())
+
+###########################################
+######### acknowledger functions ##########
+###########################################
+"""
+    /read <id>
+    - acknowledge received and read message with ID <id>
+"""
+@bot.message_handler(commands = ['read'])
+def read(message):
+    target_id = re.match('/read (\d+)', message.text).group(1)
+
+    # catch non-numeric ID
+    if target_id == None:
+        bot.reply_to(message, 'The message ID should be made of numbers only.')
+        return
+
+    # catch bad message ID
+    if target_id not in announcementRead:
+        bot.reply_to(message, 'Couldn\'t find a message with this ID.')
+        return
+    
+    if read[target_id][whoIs(message.from_user.id)] == False:
+        read[target_id][whoIs(message.from_user.id)] = True
+        bot.reply_to(message, 'Thanks! You have acknowledged message ID %d' % target_id)
+    elif read[target_id][whoIs(message.from_user.id)] == True:
+        bot.reply_to(message, 'You\'ve already read message ID %d' % target_id)
+
+    return
+
+"""
+    /status <id>
+    - returns acknowledgement status of message with ID <id>
+"""
+@bot.message_handler(commands = ['status'])                                                  
+def status(message):
+    target_id = re.match('/status (\d+)', message.text).group(1)
+
+    # catch non-numeric ID
+    if target_id == None:
+        bot.reply_to(message, 'The message ID should be made of numbers only.')
+        return
+
+    # catch bad message ID
+    if target_id not in read:
+        bot.reply_to(message, 'Couldn\'t find a message with this ID.')
+        return
+   
+    # replies for each field
+    seen = 'Acknowledged by:\n'
+    notseen = 'Not seen by:\n'
+    failed = 'Failed to send to:\n'
+
+    # counters for serial numbers for each field
+    counter['seen'] = 1
+    counter['notseen'] = 1
+    counter['failed'] = 1
+
+    for recipient in read[target_id]:
+        if read[target_id][recipient] == False:
+            notseen += '%d. %-14s' % (counter['notseen'], recipient)
+            counter['notseen'] += 1
+            if (counter['notseen']+1) % 2 == 0:
+                notseen += '\n'
+        elif read[target_id][recipient] == True:
+            seen += '%d. %-14s' % (counter['seen'], recipient)
+            counter['seen'] += 1
+            if (counter['seen']+1) % 2 == 0:
+                seen += '\n'
+        elif read[target_id][recipient] == 'Failed':
+            failed += '%d. %-14s' % (counter['failed'], recipient)
+            counter['failed'] += 1
+            if (counter['failed']+1) % 2 == 0:
+                failed += '\n'
+    
+    # append none if no one in those groups
+    if seen = 'Acknowledged by:\n':
+        seen += '-- none --'
+    if notseen = 'Not seen by:\n':
+        notseen += '-- none --'
+    if failed = 'Failed to send to: by:\n':
+        failed += '-- none --'
+
+    bot.reply_to(message, seen + '\n' + notseen + '\n' failed)
+    return
 
 logger.info('AndreaBot is listening ...')
 bot.polling()
